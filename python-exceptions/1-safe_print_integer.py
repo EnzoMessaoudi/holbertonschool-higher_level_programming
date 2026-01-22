823330e27fbe7fe44a1392b90fe2e03bdf39babf
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-22 10:24:53 +0100

    Fix code

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,8 @@
 
 def safe_print_integer(value):
 
-    while True:
-        try:
-            print("{:d}".format(value))
-            return True
-        except ValueError:
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
