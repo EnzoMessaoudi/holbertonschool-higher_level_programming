1210cb8e6b4983fc32c1eee1fea5ccf6dbf2b30d
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-23 15:46:20 +0100

    Fix code

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,12 +10,16 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """
-    Function tests
-    """
-    def test_max_value(self):
-        self.assertAlmostEqual(max_integer([1, 2, 3, 4, 5]), 5)
-        self.assertAlmostEqual(max_integer([-1, -6, -5, -8]), -1)
+
+    def test_result(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer([-1, -6, -5, -8]), -1)
+        self.assertEqual(max_integer([7]), 7)
+        self.assertEqual(max_integer([-10, 5, 3, -2, 0]), 5)
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+        self.assertEqual(max_integer([10, 2, 3, 4]), 10)
+        self.assertEqual(max_integer([1000000, 999999, 13455001]), 13455001)
+        self.assertEqual(max_integer([0, 0, 0]), 0)
 
     def test_Errors(self):
         self.assertIsNone(max_integer([]))
