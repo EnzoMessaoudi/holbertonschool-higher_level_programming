5f71fd471183278684c3a3a88fbae66d3ba1658d
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-23 09:32:33 +0100

    ADD first task

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""
+modulo that takes two integer (a and b) and add them
+>>> add_integer(2, 2)
+4
+"""
+
+
+def add_integer(a, b=98):
+    """
+    This function is use to adds 2 integers together
+    """
+    if isinstance(a, (int, float)):
+        a = int(a)
+    else:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)):
+        b = int(b)
+        return a + b
+    else:
+        raise TypeError("b must be an integer")
