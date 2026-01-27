a873b85e3cb5be5b9fb87a3a5f40254b0e6da900
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-27 11:35:53 +0100

    ADD first task

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+    This module create a new class wich is name is square and defines a square.
+"""
+
+
+class Square:
+    """
+    This create an empty class names square
+    """
+    pass
