4d0f2a1881f24e82f0fc04996b98781c2499b974
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-27 11:56:13 +0100

    ADD second task

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+Write a class square and define the size of a square
+"""
+
+
+class Square:
+    """
+    Create the size of a square
+    """
+    def __init__(self, size):
+        self._square__size = size
