10f2cb3f5096ce6eb36bed1e80aa3495d7819120
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-28 08:48:01 +0100

    ADD documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,7 +7,9 @@ Write a class square and define the size of a square
 
 
 class Square:
-
+    """
+    Create a square with a size
+    """
     def __init__(self, size):
         """
         Create the size of a square
