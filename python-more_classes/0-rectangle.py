4158611da2157521cbbee8bc084a76f9601b3b42
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-28 14:08:14 +0100

    ADD first task

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+Modulo that creates a class wich name is Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Class Named Rectangle that defines a Rectangle, empty for now
+    """
+    pass
