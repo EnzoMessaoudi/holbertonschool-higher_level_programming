356ab8a4aa144886b8350b6ef3b450070e1a9cca
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-29 10:03:53 +0100

    Fix pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -79,7 +79,8 @@ class Rectangle:
             return ""
         lines = []
         for i in range(self._Rectangle__height):
-            lines.append("{}".format(self.print_symbol) * self._Rectangle__width)
+            lines.append("{}".format(self.print_symbol)
+                         * self._Rectangle__width)
         return "\n".join(lines)
 
     def __repr__(self):
