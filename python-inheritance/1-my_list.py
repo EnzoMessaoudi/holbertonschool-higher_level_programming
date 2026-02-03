1c1c0fe3f4b95b966e0b9412d745bfc20017fdfe
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-03 09:48:51 +0100

    ADD second task

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+Modulo that permit to a class to inherit from another
+"""
+
+
+class MyList (list):
+    """
+    MyList inherit from a list and can print a list in order
+    """
+    def print_sorted(self):
+        print(sorted(self))
