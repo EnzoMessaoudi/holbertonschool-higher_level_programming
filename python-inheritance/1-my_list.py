025a4f2347f2b2e07ae4926893802d56e5def2d0
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-04 08:21:59 +0100

    Fix task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
+
+
 """
-Module that defines MyList class, which inherits from list
+Modulo that permit to a class to inherit from another
 """
 
 
 class MyList(list):
-    """MyList inherits from list and can print a sorted list"""
-
+    """
+    MyList inherit from a list and can print a list in order
+    """
     def print_sorted(self):
-        """Prints the list in ascending order without modifying it"""
+        """
+        Print a sorted list
+        """
         print(sorted(self))
-
-    def append(self, item):
-        """Append only if item is an integer"""
-        if not isinstance(item, int):
-            raise TypeError("Only integers can be added")
-        super().append(item)
-
-    def extend(self, iterable):
-        """Extend only with integers"""
-        for item in iterable:
-            if not isinstance(item, int):
-                raise TypeError("Only integers can be added")
-        super().extend(iterable)
-
-    def insert(self, index, item):
-        """Insert only if item is an integer"""
-        if not isinstance(item, int):
-            raise TypeError("Only integers can be added")
-        super().insert(index, item)
