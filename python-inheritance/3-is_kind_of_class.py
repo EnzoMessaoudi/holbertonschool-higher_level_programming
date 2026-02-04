2a8415f514b80a6ea5e967223364c6a70f2b65bd
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-04 06:28:52 +0100

    ADD comment

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -7,4 +7,7 @@ Modulo that check if an instance belong to a class or subclass
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Function that check if an object belong to a subclass
+    """
     return isinstance(obj, a_class)
