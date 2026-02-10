181d8f33b31d1709057b4ffbd6cd91fbab5c3f32
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-10 13:06:45 +0100

    ADD task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        attr_order = ["age", "last_name", "first_name"]
+        result = {}
+
+        if attrs is None:
+            for attr in attr_order:
+                result[attr] = getattr(self, attr)
+        else:
+            for attr in attr_order:
+                if attr in attrs:
+                    result[attr] = getattr(self, attr)
+
+        return result
