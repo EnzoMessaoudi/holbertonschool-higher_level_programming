73b6a3acd377fd1ca4668ad5575893b8e30899dc
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-11 08:40:57 +0100

    ADD comments

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 
+"""
+Modulo that serialize and deserialize
+"""
+
 import pickle
 
+
 def serialize_and_save_to_file(data, filename):
+    """
+    Function that serialize
+    """
     with open(filename, "wb") as file:
         pickle.dump(data, file)
 
+
 def load_and_deserialize(filename):
+    """
+    Function that deserialize
+    """
     with open(filename, "rb") as file:
         return pickle.load(file)
