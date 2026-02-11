9f9dbb22afb8462370a395ab2e40058d68651b08
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-11 08:35:42 +0100

    Fix code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     """
     Function that read a file
     """
-    with open(filename, encoding="utf-8") as file:
-        print(file.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
