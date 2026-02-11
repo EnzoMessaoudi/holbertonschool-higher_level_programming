d09df5dd57cba5db4dabdccda0040c5bedeab07e
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-11 14:07:33 +0100

    Fix code

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,20 +4,20 @@
 Modulo that serialize and deserialize
 """
 
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """
     Function that serialize
     """
-    with open(filename, "w") as file:
-        pickle.dump(data, file)
+    with open(filename, "wb", encoding='utf-8') as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """
     Function that deserialize
     """
-    with open(filename, "r") as file:
-        return pickle.load(file)
+    with open(filename, "rb", encoding='utf-8') as file:
+        return json.load(file)
