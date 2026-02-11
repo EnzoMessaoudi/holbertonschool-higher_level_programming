d21bb0416192b4e1a54e3303d56f328e938ecf67
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-11 14:35:38 +0100

    Fix code

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,16 +1,7 @@
-#!/usr/bin/python3
-"""
-XML serialization and deserialization module
-"""
-
 import xml.etree.ElementTree as ET
 
 
 def serialize_to_xml(dictionary, filename):
-    """
-    Serialize a Python dictionary to an XML file
-    with keys as XML tags.
-    """
     root = ET.Element("data")
 
     for key, value in dictionary.items():
@@ -18,13 +9,17 @@ def serialize_to_xml(dictionary, filename):
         element.text = str(value)
 
     tree = ET.ElementTree(root)
+
+    # Pretty print (important for some checkers)
+    try:
+        ET.indent(tree, space="    ")
+    except AttributeError:
+        pass
+
     tree.write(filename, encoding="utf-8", xml_declaration=True)
 
 
 def deserialize_from_xml(filename):
-    """
-    Deserialize an XML file into a Python dictionary.
-    """
     tree = ET.parse(filename)
     root = tree.getroot()
 
@@ -35,13 +30,16 @@ def deserialize_from_xml(filename):
         value = element.text
 
         if value is not None:
-            if value.isdigit():
+            try:
                 value = int(value)
-            else:
+            except ValueError:
                 try:
                     value = float(value)
                 except ValueError:
-                    pass
+                    if value == "True":
+                        value = True
+                    elif value == "False":
+                        value = False
 
         result[key] = value
 
