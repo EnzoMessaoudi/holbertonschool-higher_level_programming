b00fdaa2218e174099072db84003da9d487d3651
EnzoMessaoudi <12168@holbertonstudents.com>
2026-02-20 09:36:28 +0100

    Add task 03

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+class SimpleHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/plain; charset=utf-8")
+            self.end_headers()
+            data = {"name": 'John', "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(b"Error 404: Endpoint not found")
+
+
+server_address = ("", 8000)
+httpd = HTTPServer(server_address, SimpleHandler)
+httpd.serve_forever()
