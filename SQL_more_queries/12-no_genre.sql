ab521a3f1b8793e6d448528b71b04e64c500d093
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-04 14:02:05 +0100

    ADD TASK 12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- SELECT all the shows that doesn't have a genre in the database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
