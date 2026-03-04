11f7f21b98f8760ab5801d2aaba18cc5137571b5
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-04 13:43:40 +0100

    ADD TASK 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Lists all shows that have one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
