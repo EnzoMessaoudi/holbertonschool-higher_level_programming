ee108b9bded3c125fc2f1d5f9f5baf87964f78f7
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-04 16:51:28 +0100

    Fix code

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,6 @@
--- SELECT all the title of series that are comedies
-SELECT title
-from tv_shows
+SELECT tv_shows.title
+FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE genre_id = (
-    SELECT id
-    FROM tv_genres
-    WHERE name = "Comedy"
-)
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title ASC;
