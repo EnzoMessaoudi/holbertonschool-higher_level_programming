54f8aa313f994178e9e886db371c70847300c06a
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-04 10:40:27 +0100

    Fix code

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- SELECT all cities that are inside of California
-SELECT * FROM cities WHERE state_id = (
+SELECT id, name FROM cities WHERE state_id = (
     SELECT id
     FROM states
     WHERE name = "California"
