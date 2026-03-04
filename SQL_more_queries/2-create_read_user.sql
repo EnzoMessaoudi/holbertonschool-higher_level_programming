6d3b977547f5ffd86055e621b60e60bf389855ec
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-04 09:30:24 +0100

    Fix code

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- CREATE a database (hbtn_0d_2) and a user(user_0d_2) with SELECT permission on it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
