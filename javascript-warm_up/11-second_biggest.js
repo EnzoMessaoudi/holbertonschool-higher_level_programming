521770a7f17c6819a110c63987ef3e40bbb8206d
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-20 13:49:59 +0100

    ADD a restriction if the biggest number appears two times

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,18 +2,18 @@
 
 const args = process.argv.slice(2);
 
-if (args.length < 1 || args.length === 1) {
+if (args.length < 2 || args.length === 1) {
   console.log('0');
 } else {
-  let biggest = args[0];
-  let second = args[0] - 1;
+  let biggest = 0;
+  let second = -Infinity;
   for (let i = 0; i < args.length; i++) {
-    if (args[i] > biggest) {
-      biggest = args[i];
+    if (args[i] > args[biggest]) {
+      biggest = i;
     }
   }
   for (let j = 0; j < args.length; j++) {
-    if (args[j] > second && args[j] !== biggest) {
+    if (args[j] > second && j !== biggest) {
       second = args[j];
     }
   }
