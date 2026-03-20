f470691e5dd197b05e6936a3555810ab84d00753
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-20 13:44:07 +0100

    ADD a restriction if the first nulber is the biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ if (args.length < 1 || args.length === 1) {
   console.log('0');
 } else {
   let biggest = args[0];
-  let second = args[0];
+  let second = args[0] - 1;
   for (let i = 0; i < args.length; i++) {
     if (args[i] > biggest) {
       biggest = args[i];
