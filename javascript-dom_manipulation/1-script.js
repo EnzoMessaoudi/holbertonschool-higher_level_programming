d944ce6e9839514b2802c3a1d69999eb8b48e67a
EnzoMessaoudi <12168@holbertonstudents.com>
2026-03-25 10:27:01 +0100

    ADD task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('header');
+
+const link = document.querySelector('#red_header');
+link.addEventListener('click', () => {
+  header.style.color = '#FF0000';
+});
